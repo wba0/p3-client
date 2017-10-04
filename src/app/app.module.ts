4e50cc34ed9f71ef6ad6aaecac11d58ef8c34cc8
wba0 <william.bonneyache@gmail.com>
2017-10-04 10:25:03 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+		AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
